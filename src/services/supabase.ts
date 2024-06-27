5e681b171b289354ebdd465c139e62cfaae6d11a
Pardeep Kumar <64978605+pardeep4u@users.noreply.github.com>
2024-06-27 21:59:54 +0530

    Update supabase.ts

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = "https://mappiqdehxxdtqexpyyc.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hcHBpcWRlaHh4ZHRxZXhweXljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ0Njk2MzYsImV4cCI6MjAyMDA0NTYzNn0.kM47im37u47hZOgd1kk6ap3mH5BAZEHWjaPTcl9ozp0";
-
-console.log(supabaseKey, supabaseUrl);
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+  ? process.env.NEXT_PUBLIC_SUPABASE_KEY
+  : "";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
